bd6d739b41cc6dc97ca1ffb2ff47e856adb5bfe9
Boris Matijasevic <b.matijasevic@levi9.com>
2018-12-07 16:48:03 +0100

    .gitignore is now working

--- JollyConsole/Form1.cs
+++ JollyConsole/Form1.cs
@@ -24,6 +24,7 @@ namespace JollyConsole
         {
             InitializeComponent();
             InitializeConsole();
+            panel1.AutoScroll = true;
         }
 
         void InitializeConsole()
